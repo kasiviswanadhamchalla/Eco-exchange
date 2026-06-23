87a26cfde0374e5b074f39f20b2f32d0bdce5965
Maheshboini-hub <maheshhboini@gmail.com>
2026-06-23 23:20:13 +0530

    implemented api/config/discovery

--- discovery-service/src/main/java/com/industry_connect/discovery_service/DiscoveryServiceApplication.java
+++ discovery-service/src/main/java/com/industry_connect/discovery_service/DiscoveryServiceApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
 	}
-
 }
 
