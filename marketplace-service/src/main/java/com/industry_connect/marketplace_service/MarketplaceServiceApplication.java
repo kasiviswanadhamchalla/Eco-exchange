90f68f76da5eb3384fcde80f5967731cbe31ae0e
kasiviswanadhamchalla <kasiviswanadhamchalla@gmail.com>
2026-06-23 17:39:47 +0530

    first commit

---
+++ marketplace-service/src/main/java/com/industry_connect/marketplace_service/MarketplaceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.industry_connect.marketplace_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketplaceServiceApplication.class, args);
+	}
+
+}
---
+++ marketplace-service/src/test/java/com/industry_connect/marketplace_service/MarketplaceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.industry_connect.marketplace_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceServiceApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
